f3e0195330daae4132a0df7c8ab7d972bd3b529e
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-31 08:45:20 +0100

    iterating through a list example

---
+++ lists_intro.py
@@ -0,0 +1,17 @@
+
+computer_parts = ["computer", 
+                   "mouse", 
+                  "keyboard",  
+                   "monitor"
+                   ]
+
+for i in computer_parts:
+      print(i)
+print()
+
+print(computer_parts[2])
+print()
+print(computer_parts[0:3])
+print()
+print(computer_parts[-1]) # This is a good way to return the last value
+                         # in a list
