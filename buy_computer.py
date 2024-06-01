55f3213fb3e3d3851d44f5e28df88576f424e8bc
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-06-01 13:35:40 +0100

    making use of the enumerate function

--- buy_computer.py
+++ buy_computer.py
@@ -9,6 +9,9 @@ computer_parts = []
 while current_choice != '0':
      if current_choice in "12345":
           print("adding {}".format(current_choice))
+          
+          
+          
           if current_choice == '1':
                   computer_parts.append("computer")
           elif current_choice == '2':
@@ -19,8 +22,10 @@ while current_choice != '0':
                   computer_parts.append("keyboard")
           elif current_choice == '5':
                  computer_parts.append("hdmi cable")
+                
+        
      else:
           print("Please add items from the list below:")
-          for part in available_parts:
-               print("{0} : {1}".format(available_parts.index(part) + 1, part))
+          for number,part in enumerate(available_parts):
+               print("{0} : {1}".format(number + 1, part))
      current_choice = input()
---
+++ enumerate.py
@@ -0,0 +1,6 @@
+for index, character in enumerate("abcdefghijklmnopqrstwvxyz"):
+    print("{} : {}".format(index + 1,character))
+
+
+# This is the enumerate function that will give us the index and the
+# name of a iterable item within a list.
