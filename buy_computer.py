9969671f8a9256c0ee13201e45973964312326dc
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-06-01 12:58:04 +0100

    1st commit for the day

--- buy_computer.py
+++ buy_computer.py
@@ -1,22 +1,26 @@
+available_parts = ["computer", 
+                   "monitor",
+                   "mouse",
+                   "keyboard",
+                   "hdmi cable"]
 current_choice = "-"
 computer_parts = []
-print(input("Please press enter to start: "))
-while current_choice != "0":
-    if current_choice in "12345":
-        print("adding {}".format(current_choice))
-        if current_choice == '1':
-            computer_parts.append("computer")
-        elif current_choice == '2':
-            computer_parts.append("monitor")
-        elif current_choice == '3':
-            computer_parts.append("mouse")
-        elif current_choice == '4':
-            computer_parts.append("keyboard")
-    else:
-        print("Please add items from the list below:")
-        print("1: computer")
-        print("2: monitor")
-        print("3: mouse")
-        print("4: keyboard")
-        print("0 to finish")
-current_choice = input()
+
+while current_choice != '0':
+     if current_choice in "12345":
+          print("adding {}".format(current_choice))
+          if current_choice == '1':
+                  computer_parts.append("computer")
+          elif current_choice == '2':
+                  computer_parts.append("monitor")
+          elif current_choice == '3':
+                  computer_parts.append("mouse")
+          elif current_choice == '4':
+                  computer_parts.append("keyboard")
+          elif current_choice == '5':
+                 computer_parts.append("hdmi cable")
+     else:
+          print("Please add items from the list below:")
+          for part in available_parts:
+               print("{0} : {1}".format(available_parts.index(part) + 1, part))
+     current_choice = input()
