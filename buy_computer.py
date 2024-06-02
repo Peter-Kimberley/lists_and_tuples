7bfba8653ed97166fe3668362c55f6c72dd003ed
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-06-02 11:00:28 +0100

    enumerate function

--- buy_computer.py
+++ buy_computer.py
@@ -2,30 +2,26 @@ available_parts = ["computer",
                    "monitor",
                    "mouse",
                    "keyboard",
-                   "hdmi cable"]
+                   "hdmi cable"
+                   ]
+# valid_chioces = [str(i) for i in range(1, len(available_parts) + 1)]
+valid_choices = []
+for i in range (1, len(available_parts) + 1):
+       valid_choices.append(str(i))
+print(valid_choices)
+input("please press enter ")
 current_choice = "-"
 computer_parts = []
 
 while current_choice != '0':
-     if current_choice in "12345":
+     if current_choice in valid_choices:
           print("adding {}".format(current_choice))
-          
-          
-          
-          if current_choice == '1':
-                  computer_parts.append("computer")
-          elif current_choice == '2':
-                  computer_parts.append("monitor")
-          elif current_choice == '3':
-                  computer_parts.append("mouse")
-          elif current_choice == '4':
-                  computer_parts.append("keyboard")
-          elif current_choice == '5':
-                 computer_parts.append("hdmi cable")
-                
-        
+          index = int(current_choice ) - 1
+          chosen_part = available_parts[index]
+          computer_parts.append(chosen_part)
      else:
-          print("Please add items from the list below:")
-          for number,part in enumerate(available_parts):
-               print("{0} : {1}".format(number + 1, part))
+        print("Please add items from the list below:")
+        for number,part in enumerate(available_parts):
+              print("{0} : {1}".format(number + 1, part))
      current_choice = input()
+print(computer_parts)
