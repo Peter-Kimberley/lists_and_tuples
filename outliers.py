012ac735432d1ebf86b0e641e91ff1f436a13f5d
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-06-04 07:22:42 +0100

    Making use of the enumerat and indexing

--- outliers.py
+++ outliers.py
@@ -1,14 +1,17 @@
 data = [4, 5, 104, 105, 110, 120, 130, 130, 150, 160, 170, 183, 185, 
         187, 188, 191, 350, 360]
-#del data[0:2] # using the delete function
-#print(data)
-#del data[14:]
-#print(data)
+
 min_valid = 100
 max_valid = 200
 
+# processing the low values from the list
+stop = 0
 for index, value in enumerate(data):
-    if (value  < min_valid) or (value > max_valid):
-       del data[index]
-       print(data)
-       
+    if value >= min_valid:
+        stop = index
+        break
+    
+print(stop)
+del data[:stop]
+print (data)
+ 
