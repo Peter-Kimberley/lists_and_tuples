b8dc0ddf8bc4797107767b2d630d1d6014dbd09a
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-06-05 21:09:20 +0100

    This is the latest from my lists and tuples from udemy course

--- number_lists.py
+++ number_lists.py
@@ -2,22 +2,13 @@ empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-numbers = even + odd
+numbers = [even , odd]
 print(numbers)
+# This is an example of the outer foorloop
+for numbers_list in numbers:
+    print(numbers_list)
+    # this is an example of a nested forloop
+    for value in numbers_list:
+        print(value)
 
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-print(numbers)
-
-
-digits= list("432985617")
-print(digits)
-
-#'more_numbers = list(numbers)
-#more_numbers = numbers[:]
-more_numbers = numbers.copy( )
-print(more_numbers)
-
-print(numbers is more_numbers)
-print(numbers == more_numbers)
 
---
+++ spam.py
@@ -0,0 +1,10 @@
+menu = [
+    ["eeg", "bacon"], 
+    ["egg", "sauasage", "bacon"]
+    ["egg", "spam"]
+    ["egg", "bacon", "spam"]
+    ["egg", "bacon", "sausage", "spam"]
+    ["spam", "bacon", "sausage", "spam"]
+    ["spam", "egg", "spam", "spam" "bacon", "spam"]
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam" ],
+    ]
